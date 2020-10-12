8a3f80e5170faff1391f71b41972de764cfac005
Kevin <jinc0008@e.ntu.edu.sg>
2020-10-13 00:46:22 +0800

    dp

---
+++ AcWing/DP/279.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<map>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+
+#define fi first
+#define se second
+#define endl '\n'
+#define pb push_back
+#define ppf pop_front
+#define ppb pop_back
+#define mp make_pair
+typedef long long ll;
+typedef std::pair<int,int> pii;
+typedef std::pair<ll,ll> pll;
+typedef std::vector<int> vi;
+typedef std::vector<pii> vii;
+typedef std::vector<std::vector<int>> vvi;
+
+using namespace std;
+
+template<class T,class T2>
+inline bool chkmax(T &x,const T2 &y){return x<y?(x=y,1):0;}
+template<class T,class T2>
+inline bool chkmin(T &x,const T2 &y){return x>y?(x=y,1):0;}
+
+int dp[4001];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int n; cin>>n;
+	dp[0]=1;
+	for (int i=1;i<n;i++) for (int j=i;j<=n;j++) 
+		dp[j]+=dp[j-i];
+	cout<<(dp[n]&(1ll<<31)-1)<<endl;
+	
+	return 0;
+}
