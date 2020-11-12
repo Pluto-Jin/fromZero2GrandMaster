847f1949afbb617a99d9884e0f4ea738720eb9d8
Kevin <jinc0008@e.ntu.edu.sg>
2020-11-13 00:19:50 +0800

    最大异或对

---
+++ AcWing/DS/143.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<map>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+#include<iomanip>
+
+#define fi first
+#define se second
+#define endl '\n'
+#define pb push_back
+#define ppf pop_front
+#define ppb pop_back
+#define mp make_pair
+typedef long long ll;
+typedef std::pair<int,int> pii;
+typedef std::pair<ll,ll> pll;
+typedef std::vector<int> vi;
+typedef std::vector<pii> vii;
+typedef std::vector<std::vector<int>> vvi;
+
+using namespace std;
+
+template<class T,class T2>
+inline bool chkmax(T &x,const T2 &y){return x<y?(x=y,1):0;}
+template<class T,class T2>
+inline bool chkmin(T &x,const T2 &y){return x>y?(x=y,1):0;}
+
+const int N=4e6+1;
+int trie[N][2],cnt[N],ind;
+
+void insert(int a) {
+	int p=0;
+	for (int i=30;~i;i--) {
+		int &x=trie[p][a>>i&1];
+		if (!x) x=++ind;
+		p=x;
+	}
+	cnt[p]++;
+}
+
+int query(int a) {
+	int p=0,res=0;
+	for (int i=30;~i;i--) {
+		int &x=trie[p][!(a>>i&1)];
+		if (x) res+=1<<i,p=x;
+		else p=trie[p][a>>i&1];
+	}
+	return res;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int n,ans=0; cin>>n;
+	for (int i=0;i<n;i++) {
+		int a; cin>>a;
+		if (i) ans=max(ans,query(a));
+		insert(a);
+	}
+	cout<<ans<<endl;
+	
+	return 0;
+}
