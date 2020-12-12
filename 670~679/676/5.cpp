ca151824d1627a562982c74e21f57de928948eac
Kevin <jinc0008@e.ntu.edu.sg>
2020-12-12 23:22:36 +0800

    Revert "676E\"
    This reverts commit 056bffc61f0d7e07f94ddd6cec69e6d27458fa9f.

--- 670~679/676/5.cpp
+++ 670~679/676/5.cpp
@@ -37,8 +37,34 @@ template<class T,class T2>
 inline bool chkmin(T &x,const T2 &y){return x>y?(x=y,1):0;}
 
 const int N=2e5+1;
-int n;
-ll dp[N][3][2][2],a[N];
+vector<pll> tmp;
+ll n,a[N],b[N],mem[N],cnt;
+
+ll check() {
+	int ok=1;
+	for (int i=1;ok and i<n;i++) if (b[i]==b[i+1]) ok=0;
+	if (n>1 and ok) {
+		if (b[tmp[0].se]==mem[tmp[0].se]) {
+			b[tmp[0].se]^=1;
+			for (int i=1;i<n;i++) if (b[tmp[i].se]==b[tmp[0].se]) {
+				b[tmp[i].se]^=1;
+				break;
+			}
+		} else {
+			int tt=b[tmp[0].se],pos;
+			for (auto [i,j]:tmp) if (b[j]==tt and b[j]!=mem[j]) pos=j;
+			for (auto [i,j]:tmp) if (b[j]!=tt) {
+				b[j]^=1;
+				break;
+			}
+			b[pos]^=1;
+		}
+	}
+	ll res=0;
+	for (int i=1;i<=n;i++) if (b[i]) res-=a[i]; else res+=a[i];
+	memcpy(b,mem,sizeof b);
+	return res;
+}
 
 int main()
 {
@@ -46,18 +72,106 @@ int main()
     cin.tie(NULL);
 
 	cin>>n;
-	for (int i=1;i<=n;i++) cin>>a[i];
-	if (n==1) {cout<<abs(a[1])<<endl;return 0;}
-	memset(dp,0xc0,sizeof dp);
-	dp[1][1][1][0]=-a[1];
-	dp[1][0][1][1]=a[1];
-	for (int i=2;i<=n;i++) for (int j=0;j<3;j++) {
-		dp[i][j][0][0]=-a[i]+max(max(dp[i-1][(j+2)%3][0][0],dp[i-1][(j+2)%3][0][1]),dp[i-1][(j+2)%3][1][0]);
-		dp[i][j][0][1]=a[i]+max(max(dp[i-1][j][0][0],dp[i-1][j][0][1]),dp[i-1][j][1][1]);
-		dp[i][j][1][0]=-a[i]+dp[i-1][(j+2)%3][1][1];
-		dp[i][j][1][1]=a[i]+dp[i-1][j][1][0];
+	tmp.resize(n);
+	for (int i=1;i<=n;i++) {
+		cin>>a[i];
+		tmp[i-1]=mp(abs(a[i]),i);
+		if (a[i]<0) mem[i]=b[i]=1,cnt++;
 	}
-	cout<<max(dp[n][(4-n%3)%3][0][0],dp[n][(4-n%3)%3][0][1])<<endl;
-
+	sort(tmp.begin(),tmp.end());
+	ll ans=-1e18;
+	if ((cnt+n)%3==0) {
+		int pos=0,neg=0;
+		for (auto [i,j]:tmp) if (!b[j]) pos++; else neg++;
+		if (pos>=1) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (!b[j]) b[j]^=1,tt++;
+				if (tt==1) break;
+			}
+			chkmax(ans,check());
+		} 
+		if (pos>=4) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (!b[j]) b[j]^=1,tt++;
+				if (tt==4) break;
+			}
+			chkmax(ans,check());
+		} 
+		if (neg>=2) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (b[j]) b[j]^=1,tt++;
+				if (tt==2) break;
+			}
+			chkmax(ans,check());
+		} 
+		if (neg>=5) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (b[j]) b[j]^=1,tt++;
+				if (tt==5) break;
+			}
+			chkmax(ans,check());
+		} 
+	} else if ((cnt+n)%3==1) {
+		chkmax(ans,check());
+		int pos=0,neg=0;
+		for (auto [i,j]:tmp) if (!b[j]) pos++; else neg++;
+		if (pos>=3) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (!b[j]) b[j]^=1,tt++;
+				if (tt==3) break;
+			}
+			chkmax(ans,check());
+		} 
+		if (neg>=3) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (b[j]) b[j]^=1,tt++;
+				if (tt==3) break;
+			}
+			chkmax(ans,check());
+		} 
+	} else {
+		int pos=0,neg=0;
+		for (auto [i,j]:tmp) if (!b[j]) pos++; else neg++;
+		if (pos>=2) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (!b[j]) b[j]^=1,tt++;
+				if (tt==2) break;
+			}
+			chkmax(ans,check());
+		} 
+		if (pos>=5) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (!b[j]) b[j]^=1,tt++;
+				if (tt==5) break;
+			}
+			chkmax(ans,check());
+		} 
+		if (neg>=1) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (b[j]) b[j]^=1,tt++;
+				if (tt==1) break;
+			}
+			chkmax(ans,check());
+		} 
+		if (neg>=4) {
+			int tt=0;
+			for (auto [i,j]:tmp) {
+				if (b[j]) b[j]^=1,tt++;
+				if (tt==4) break;
+			}
+			chkmax(ans,check());
+		} 
+	}
+	cout<<ans<<endl;
+	
 	return 0;
 }
