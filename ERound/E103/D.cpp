85f0cece87057aa2c63005b63db615cfb74dba93
Kevin <jinc0008@e.ntu.edu.sg>
2021-02-07 23:27:50 +0800

    E103

---
+++ ERound/E103/D.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<map>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<numeric>
+#include<cstdlib>
+#include<ctime>
+#include<iomanip>
+
+#define fi first
+#define se second
+#define endl '\n'
+#define pb push_back
+#define eb emplace_back
+#define ppf pop_front
+#define ppb pop_back
+typedef long long ll;
+typedef std::pair<int,int> pii;
+typedef std::pair<ll,ll> pll;
+typedef std::vector<int> vi;
+typedef std::vector<pii> vii;
+typedef std::vector<std::vector<int>> vvi;
+
+using namespace std;
+
+template<class T,class T2>
+inline bool chkmax(T &x,const T2 &y){return x<y?(x=y,1):0;}
+template<class T,class T2>
+inline bool chkmin(T &x,const T2 &y){return x>y?(x=y,1):0;}
+
+const int N=3e5+5;
+int a[N],dp[N][2];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int t; cin>>t;
+	while (t--) {
+		int n; cin>>n;
+		string s; cin>>s;
+		for (int i=1;i<=n;i++) a[i]=(s[i-1]=='L');
+		for (int i=n;i;i--) if (!a[i]) {
+			dp[i][0]=1;
+			if (i+1<=n and a[i+1]) {
+				dp[i][0]++;
+				if (i+2<=n) dp[i][0]+=dp[i+2][0];
+			}
+		} else dp[i][0]=0;	
+		for (int i=1;i<=n;i++) if (a[i]) {
+			dp[i][1]=1;
+			if (i-1 and !a[i-1]) {
+				dp[i][1]++;
+				if (i-2) dp[i][1]+=dp[i-2][1];
+			}
+		} else dp[i][1]=0;
+		cout<<dp[1][0]+1<<' ';
+		for (int i=2;i<=n;i++) cout<<dp[i][0]+dp[i-1][1]+1<<' ';
+		cout<<dp[n][1]+1<<endl;
+	}
+	
+	return 0;
+}
