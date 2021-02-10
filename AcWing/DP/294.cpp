30dffd6715b6a112717c35f9ed71945827bc46d7
Kevin <jinc0008@e.ntu.edu.sg>
2021-02-10 21:43:01 +0800

    倍增优化

---
+++ AcWing/DP/294.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<map>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<numeric>
+#include<cstdlib>
+#include<ctime>
+#include<iomanip>
+
+#define fi first
+#define se second
+#define endl '\n'
+#define pb push_back
+#define eb emplace_back
+#define ppf pop_front
+#define ppb pop_back
+typedef long long ll;
+typedef std::pair<int,int> pii;
+typedef std::pair<ll,ll> pll;
+typedef std::vector<int> vi;
+typedef std::vector<pii> vii;
+typedef std::vector<std::vector<int>> vvi;
+
+using namespace std;
+
+template<class T,class T2>
+inline bool chkmax(T &x,const T2 &y){return x<y?(x=y,1):0;}
+template<class T,class T2>
+inline bool chkmin(T &x,const T2 &y){return x>y?(x=y,1):0;}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	string s[2];
+	int a[2], dp[100][31];
+	while (cin>>s[0]>>a[0]) {
+		cin>>s[1]>>a[1];
+		int n[]={s[0].size(),s[1].size()};
+		for (int i=0;i<n[0];i++) {
+			int cnt=0,tmp=i;
+			for (int j=0;j<n[1];j++)
+				if (s[0][tmp]==s[1][j]) tmp++,tmp%=n[0],cnt++;
+			dp[i][0]=cnt;
+		}
+		for (int j=1;j<31;j++) for (int i=0;i<n[0];i++) 
+			dp[i][j]=dp[i][j-1]+dp[(i+dp[i][j-1])%n[0]][j-1];
+		int ans=0;
+		for (int i=30,j=0;~i;i--) if (a[1]>>i&1) {
+			ans+=dp[j][i];
+			j+=dp[j][i],j%=n[0];
+		}
+		cout<<(ans/n[0]/a[0])<<endl;
+	}
+	
+	return 0;
+}
