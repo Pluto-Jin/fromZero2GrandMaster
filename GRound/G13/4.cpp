55c895090891b2900f736f79e3c0f3c9a4e45ae0
Kevin <jinc0008@e.ntu.edu.sg>
2021-03-02 22:44:44 +0800

    G13

---
+++ GRound/G13/4.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<set>
+#include<map>
+#include<bitset>
+#include<string>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<numeric>
+#include<cstdlib>
+#include<ctime>
+#include<iomanip>
+
+#define fi first
+#define se second
+#define endl '\n'
+#define pb push_back
+#define eb emplace_back
+#define ppf pop_front
+#define ppb pop_back
+typedef long long ll;
+typedef std::pair<int,int> pii;
+typedef std::pair<ll,ll> pll;
+typedef std::vector<int> vi;
+typedef std::vector<pii> vii;
+typedef std::vector<std::vector<int>> vvi;
+
+using namespace std;
+
+template<class T,class T2>
+inline bool chkmax(T &x,const T2 &y){return x<y?(x=y,1):0;}
+template<class T,class T2>
+inline bool chkmin(T &x,const T2 &y){return x>y?(x=y,1):0;}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	int t; cin>>t;
+	while (t--) {
+		int x,y; cin>>x>>y;
+		if (x>y or __builtin_popcount(y)>__builtin_popcount(x)) cout<<"NO"<<endl;
+		else {
+			int a=0,b=0,ok=1;
+			for (int i=0;ok and i<30;i++) {
+				b+=y>>i&1,a+=x>>i&1;
+				if (b>a) ok=0;
+			}
+			cout<<(ok?"YES":"NO")<<endl;
+		}
+	}
+	
+	return 0;
+}
